4098f3ccc8b71c229d4c0bbdd432416891675c58
panda <91168831+pandacoder123@users.noreply.github.com>
2023-04-10 19:33:46 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+const cacheName = 'my-site-cache-v1';
+const urlsToCache = [
+  '/calc/',
+  '/calc/terms.html',
+  '/calc/readme.md'
+  '/calc/icon/fox.png',
+  '/calc/index.js',
+  '/calc/style.css',
+  '/calc/icon/fox.png',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request);
+      })
+  );
+});
